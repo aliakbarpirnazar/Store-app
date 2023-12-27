4a3bb1b12945b1032a5fd2de1286dfd01b8d45f4
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2023-12-27 13:16:56 +0330

    fetch data products

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import { getCookie } from "./utils/cookie.js";
+import { getData } from "./utils/httpReq.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
 
-const init = () => {
+const showProducts = (product) => {};
+
+const init = async () => {
   const cookie = getCookie();
   if (cookie) {
     loginButton.style.display = "none";
   } else {
     dashboardButton.style.display = "none";
   }
+
+  const allProducts = await getData("products");
+  showProducts(allProducts);
 };
 
 document.addEventListener("DOMContentLoaded", init);
--- utils/httpReq.js
+++ utils/httpReq.js
@@ -16,4 +16,14 @@ const postData = async (path, data) => {
   }
 };
 
-export { postData };
+const getData = async (path) => {
+  try {
+    const response = await fetch(`${BASE_URL}/${path}`);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    alert("An error occured!");
+  }
+};
+
+export { postData, getData };
